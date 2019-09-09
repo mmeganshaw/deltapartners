1aedb91fd29864b2f5ab48f79e640b837c46cecb
Megan Shaw <meganrose@Megans-MacBook-Air.local>
2019-09-09 13:55:34 -0400

    Writing scripts for form submission.

--- public/js/init.js
+++ public/js/init.js
@@ -4,6 +4,7 @@
       $('.sidenav').sidenav();
       $('.carousel').carousel();
       $('.collapsible').collapsible();
+      $('select').formSelect();
   
     }); // end of document ready
   })(jQuery); // end of jQuery name space
